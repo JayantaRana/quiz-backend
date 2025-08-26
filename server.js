bfe17207648431a6d3a00c7bd48aea2ce9a23812
JayantaRana <113259493+JayantaRana@users.noreply.github.com>
2025-08-26 18:53:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// const quizRoutes = require("./routes/quizRoutes");
+
+
+
+
+
+// dotenv.config();
+// const app = express();
+// app.use(express.json());
+
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB connected"))
+//   .catch(err => console.log(err));
+
+// app.use("/api/quiz", quizRoutes);
+
+// app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+
+
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const quizRoutes = require("./routes/quizRoutes");
 
-
-
-
-
 dotenv.config();
 const app = express();
 app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
 app.use("/api/quiz", quizRoutes);
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = app; // Export app for Vercel
